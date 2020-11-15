fa0dce31ba3272e11c1069ac3303ffa9fdb51499
peyman simon hewitt <shewitt@live.co.uk>
2020-11-15 20:53:02 +0000

    2 methods

--- unitconvert.py
+++ unitconvert.py
@@ -57,3 +57,21 @@ elif unit1 == "m" and unit2 == "cm":
 #allow user input fields to take in values, drop down menus/or input for each unit type
 
 # build instructions, pip pip install pipenv, python unitconvert.py
+# def conversion():
+#     user_input = input('Please input the desired conversion (X cm/m in cm/m): ')
+#     try:
+#         user_input = user_input.split()
+#         from_unit = user_input[1]
+#         to_unit = user_input[3]
+#         value = int(user_input[0])
+
+#         if (from_unit == 'cm' and to_unit == 'm'):
+#             return print(str(value/100) + 'm')
+#         elif (from_unit == 'm' and to_unit == 'cm'):
+#             return print(str(value * 100) + 'cm')
+
+#     except:
+#         print('Please request a valid conversion')
+#         conversion()
+
+# conversion()
